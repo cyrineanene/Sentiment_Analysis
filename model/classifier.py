bb78d42c6205d15651401a4498c65671678cbdb5
cyrineanene <cyrine.anene@supcom.tn>
2024-04-12 16:00:08 +0100

    evaluation added

--- model/classifier.py
+++ model/classifier.py
@@ -2,11 +2,10 @@ import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-
 class TextModel:
     def __init__(self, max_features=5000):
         self.vectorizer = TfidfVectorizer(max_features=max_features)
-        self.classifier = MultinomialNB()
+        self.classifier = MultinomialNB() #the algorithm
         
     def train(self, X_train, y_train):
         self.classifier.fit(X_train, y_train)
@@ -21,7 +20,6 @@ class TextModel:
     def load(self, vectorizer_path, model_path):
         self.vectorizer = pickle.load(open(vectorizer_path, 'rb'))
         self.classifier = pickle.load(open(model_path, 'rb'))
-
 
 class SentimentAnalyzer:
     def __init__(self, vectorizer_path, model_path):
